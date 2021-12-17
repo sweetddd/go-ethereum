2bb055f02a6c50d0d8b34daa542f857eb8e91b81
maskpp <32827930+mask-pp@users.noreply.github.com>
2021-12-17 13:56:40 +0800

    Disable consensus and close p2p service. (#8)
    * stop p2p and sync handler
    
    * disable clique consensus create block
    
    * Close useless code about p2p settings.

--- miner/worker.go
+++ miner/worker.go
@@ -498,10 +498,10 @@ func (w *worker) newWorkLoop(recommit time.Duration) {
 			timestamp = time.Now().Unix()
 			commit(false, commitInterruptNewHead)
 
-		case head := <-w.chainHeadCh:
+		/*case head := <-w.chainHeadCh:
 			clearPending(head.Block.NumberU64())
 			timestamp = time.Now().Unix()
-			commit(false, commitInterruptNewHead)
+			commit(false, commitInterruptNewHead)*/
 
 		case <-timer.C:
 			// If sealing is running resubmit a new work cycle periodically to pull in
