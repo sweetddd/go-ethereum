242ad5c430d0795555a67dbcf469ef6ec44dd099
maskpp <maskpp266@gmail.com>
2022-12-13 11:45:43 +0800

    chore(ws): increase ws BufferSize & MessageSizeLimit (#192)
    Increase ws cache in rpc module

--- rpc/websocket.go
+++ rpc/websocket.go
@@ -33,12 +33,12 @@ import (
 )
 
 const (
-	wsReadBuffer       = 1024
-	wsWriteBuffer      = 1024
+	wsReadBuffer       = 3*1024
+	wsWriteBuffer      = 3*1024
 	wsPingInterval     = 30 * time.Second
 	wsPingWriteTimeout = 5 * time.Second
 	wsPongTimeout      = 30 * time.Second
-	wsMessageSizeLimit = 32 * 1024 * 1024
+	wsMessageSizeLimit = 10* 32 * 1024 * 1024
 )
 
 var wsBufferPool = new(sync.Pool)
